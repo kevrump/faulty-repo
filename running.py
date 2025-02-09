1108a9f39cbcf1b54e43f1d7f63c82febdd865b7
kevrump <116436680+kevrump@users.noreply.github.com>
2025-02-09 19:20:52 +0100

    Auto-fix issue

--- running.py
+++ running.py
@@ -1,28 +1,32 @@
 import requests
 import time
+import asyncio
+import aiohttp
 
 
-def running():
+async def fetch(session, url):
+    async with session.get(url) as response:
+        if response.status == 200:
+            return await response.json()
+
+
+async def running():
     urls = ["https://jsonplaceholder.typicode.com/posts/1"] * 5
-    responses = []
-    for url in urls:
-        response = requests.get(url)
-        if response.status_code == 200:
-            responses.append(response.json())
+    async with aiohttp.ClientSession() as session:
+        responses = await asyncio.gather(*[fetch(session, url) for url in urls])
 
     data = [i for i in range(5000)]
-    processed_data = []
-    for item in data:
-        if item not in processed_data:
-            processed_data.append(item)
+    processed_data = list(dict.fromkeys(data))
 
-    large_list_1 = list(range(3000))
-    large_list_2 = list(range(2000, 5000))
-    common_elements = []
-    for item in large_list_1:
-        if item in large_list_2:
-            common_elements.append(item)
+    large_list_1 = set(range(3000))
+    large_list_2 = set(range(2000, 5000))
+    common_elements = list(large_list_1 & large_list_2)
 
-    time.sleep(2)
+    await asyncio.sleep(2)
 
     return {"responses": responses, "common_elements": common_elements}
+
+
+def running_sync():
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(running())
